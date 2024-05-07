3de48cc9f7339658b698a34b12551180edea3348
Arfara Yema S <indonesianstudent14@gmail.com>
2024-05-07 22:00:33 +0700

    refreshment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Flex,
-  HStack,
-  IconButton,
-  Input,
-  SkeletonText,
-  Text,
-} from '@chakra-ui/react'
-import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+import { Box, Button, ButtonGroup, Flex, HStack, IconButton, Input, Menu, MenuButton, MenuItem, MenuList, SkeletonText, Text } from '@chakra-ui/react'
+import { FaLocationArrow, FaTimes, FaCaretDown } from 'react-icons/fa'
+import CompanyLogo from './Assets/magna.png';
 
-import {
-  useJsApiLoader,
-  GoogleMap,
-  Marker,
-  Autocomplete,
-  DirectionsRenderer,
-} from '@react-google-maps/api'
+import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer } from '@react-google-maps/api'
 import { useRef, useState } from 'react'
 
-const center = { lat: 48.8584, lng: 2.2945 }
+const center = { lat: -6.241193693118617, lng: 106.834407451799 }
 
 function App() {
   const { isLoaded } = useJsApiLoader({
@@ -28,31 +13,27 @@ function App() {
     libraries: ['places'],
   })
 
-  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
+  const [map, setMap] = useState(null)
   const [directionsResponse, setDirectionsResponse] = useState(null)
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
 
-  /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef()
-  /** @type React.MutableRefObject<HTMLInputElement> */
-  const destiantionRef = useRef()
+  const destinationRef = useRef()
 
   if (!isLoaded) {
     return <SkeletonText />
   }
 
   async function calculateRoute() {
-    if (originRef.current.value === '' || destiantionRef.current.value === '') {
+    if (!originRef.current.value || !destinationRef.current.value) {
       return
     }
-    // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService()
+    const directionsService = new window.google.maps.DirectionsService()
     const results = await directionsService.route({
       origin: originRef.current.value,
-      destination: destiantionRef.current.value,
-      // eslint-disable-next-line no-undef
-      travelMode: google.maps.TravelMode.DRIVING,
+      destination: destinationRef.current.value,
+      travelMode: window.google.maps.TravelMode.DRIVING,
     })
     setDirectionsResponse(results)
     setDistance(results.routes[0].legs[0].distance.text)
@@ -64,19 +45,44 @@ function App() {
     setDistance('')
     setDuration('')
     originRef.current.value = ''
-    destiantionRef.current.value = ''
+    destinationRef.current.value = ''
   }
 
   return (
     <Flex
-      position='relative'
       flexDirection='column'
       alignItems='center'
       h='100vh'
       w='100vw'
+      overflowX='hidden'
     >
-      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
-        {/* Google Map Box */}
+      {/* Header */}
+      <Flex
+        alignItems='center'
+        justifyContent='space-between'
+        w='100%'
+        p={4}
+        maxWidth='100%'
+      >
+        {/* Logo */}
+        <Box>
+          <img src={CompanyLogo} alt="Company Logo" style={{ width: '80px', height: '50px' }} />
+        </Box>
+        {/* Dropdown Try Another APIs */}
+        <Menu>
+          <MenuButton as={Button} rightIcon={<FaCaretDown />} variant='outline' colorScheme='blue'>
+            Try Another APIs
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Geocoding API (under development)</MenuItem>
+            <MenuItem>Geolocation API (under development)</MenuItem>
+            <MenuItem>Streetview API (under development)</MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+
+      {/* Google Map Box */}
+      <Box position='relative' flex='1' width='100%' maxWidth='100%' overflow='hidden'>
         <GoogleMap
           center={center}
           zoom={15}
@@ -95,42 +101,45 @@ function App() {
           )}
         </GoogleMap>
       </Box>
+
+      {/* Form untuk input origin dan destination */}
       <Box
         p={4}
         borderRadius='lg'
         m={4}
         bgColor='white'
         shadow='base'
-        minW='container.md'
-        zIndex='1'
+        minWidth='90%'
+        maxWidth='90%'
       >
-        <HStack spacing={2} justifyContent='space-between'>
-          <Box flexGrow={1}>
+        <Flex flexDirection={['column', 'row']} gap='5'>
+          <Box flex={['none', '1']}>
             <Autocomplete>
               <Input type='text' placeholder='Origin' ref={originRef} />
             </Autocomplete>
           </Box>
-          <Box flexGrow={1}>
+          <Box flex={['none', '1']} mt={[4, 0]}>
             <Autocomplete>
               <Input
                 type='text'
                 placeholder='Destination'
-                ref={destiantionRef}
+                ref={destinationRef}
               />
             </Autocomplete>
           </Box>
-
-          <ButtonGroup>
-            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
-              Calculate Route
-            </Button>
-            <IconButton
-              aria-label='center back'
-              icon={<FaTimes />}
-              onClick={clearRoute}
-            />
-          </ButtonGroup>
-        </HStack>
+          <Box mt={[4, 0]}>
+            <ButtonGroup>
+              <Button colorScheme='blue' type='submit' onClick={calculateRoute}>
+                Calculate Route
+              </Button>
+              <IconButton
+                aria-label='center back'
+                icon={<FaTimes />}
+                onClick={clearRoute}
+              />
+            </ButtonGroup>
+          </Box>
+        </Flex>
         <HStack spacing={4} mt={4} justifyContent='space-between'>
           <Text>Distance: {distance} </Text>
           <Text>Duration: {duration} </Text>
